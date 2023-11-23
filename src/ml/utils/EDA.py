fd90265d0b781d0fea7d14225d8da0360ef84584
onetwothr1 <kimjh1101@snu.ac.kr>
2023-11-23 17:14:46 +0000

    Merged check_samplerate into EDA.py

--- src/ml/utils/EDA.py
+++ src/ml/utils/EDA.py
@@ -1,4 +1,3 @@
-
 import math, random
 import torch
 import torchaudio
@@ -7,6 +6,7 @@ from IPython.display import Audio
 from matplotlib import pyplot as plt
 from glob import glob
 from tqdm import tqdm
+import wave
 
 
 def audio_length_distibution(data_dir, ext='wav'):
@@ -16,5 +16,22 @@ def audio_length_distibution(data_dir, ext='wav'):
         ms_list.append(sig.size(1)/sr)
     ms_list.sort()
     plt.hist(ms_list, bins=200)
+    plt.show()
 
+def check_samplerate(data_dir, ext='wav'):
+    sr_list = []
+    for file_name in tqdm(glob(data_dir + '/*.' + ext)):
+        with wave.open(data_dir + '/' + file_name, "rb") as wave_file:
+            sr_list.append(wave_file.getframerate())
+
+    all_same = all(sr == sr_list[0] for sr in sr_list)
+    if all_same:
+        print("SAMPLE RATE: ", sr_list[0])
+    else:
+        print("SAMPLE RATEs are not equal")
+        plt.hist(sr_list)
+        plt.xlabel('sample rate')
+        plt.ylabel('num data')
+        plt.show()
+            
 audio_length_distibution('/home/elicer/project/data/raw/audio-mnist-whole')
--- src/ml/utils/check_samplerate.py
+++
@@ -1,8 +0,0 @@
-import os
-import wave
-
-data_dir = ''
-for file_name in os.listdir(data_dir):
-    with wave.open(data_dir + '/' + file_name, "rb") as wave_file:
-        frame_rate = wave_file.getframerate()
-        print(frame_rate)
