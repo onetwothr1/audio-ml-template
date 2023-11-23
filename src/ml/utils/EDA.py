af3bcd26f6af05c384ad5c6f31573219c65a9ba8
onetwothr1 <kimjh1101@snu.ac.kr>
2023-11-23 18:12:09 +0000

    Integrated multiple functions into one

--- src/ml/utils/EDA.py
+++ src/ml/utils/EDA.py
@@ -1,3 +1,4 @@
+#%%
 import math, random
 import torch
 import torchaudio
@@ -8,30 +9,54 @@ from glob import glob
 from tqdm import tqdm
 import wave
 
+data_dir = '/home/elicer/project/data/raw/audio-mnist-whole'
+ext = 'wav'
 
-def audio_length_distibution(data_dir, ext='wav'):
-    ms_list = []
+def EDA():
+    ms_list = [] # audio length in milisec
+    sr_list = [] # sample rate
+    ch_list = [] # channel num
     for file_name in tqdm(glob(data_dir + '/*.' + ext)):
         sig, sr = torchaudio.load(file_name)
         ms_list.append(sig.size(1)/sr)
-    ms_list.sort()
+        sr_list.append(sr)
+        ch_list.append(sig.size(0))
     plt.hist(ms_list, bins=200)
+    plt.xlabel('ms')
+    plt.ylabel('num data')
+    plt.title('audio lenth distribution')
     plt.show()
 
-def check_samplerate(data_dir, ext='wav'):
-    sr_list = []
-    for file_name in tqdm(glob(data_dir + '/*.' + ext)):
-        with wave.open(data_dir + '/' + file_name, "rb") as wave_file:
-            sr_list.append(wave_file.getframerate())
-
-    all_same = all(sr == sr_list[0] for sr in sr_list)
-    if all_same:
+    sr_all_same = all(sr == sr_list[0] for sr in sr_list)
+    if sr_all_same:
         print("SAMPLE RATE: ", sr_list[0])
     else:
         print("SAMPLE RATEs are not equal")
         plt.hist(sr_list)
         plt.xlabel('sample rate')
         plt.ylabel('num data')
+        plt.title('samplerate')
         plt.show()
-            
-audio_length_distibution('/home/elicer/project/data/raw/audio-mnist-whole')
+
+
+    ch_all_same = all(ch == ch_list[0] for ch in ch_list)
+    if ch_all_same:
+        print("CHANNEL: ", ch_list[0])
+    else:
+        mono = 0
+        stereo = 0
+        for i in ch_list:
+            if i==1:
+                mono += 1
+            elif i==2:
+                stereo += 1
+            else:
+                print('channel should be 1 or 2 but got channel size of ', i)
+                break
+        print("CHANNELs are not equal")
+        print('mono: ', mono)
+        print('stereo', stereo)
+
+if __name__=='__main__':
+    EDA()
+# %%
