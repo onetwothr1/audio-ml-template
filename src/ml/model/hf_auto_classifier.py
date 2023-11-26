84dba393663f7b3335291054dd39893797004dc1
onetwothr1 <kimjh1101@snu.ac.kr>
2023-11-26 20:11:41 +0000

    fix: able to change hugging face model

--- src/ml/model/hf_auto_classifier.py
+++ src/ml/model/hf_auto_classifier.py
@@ -2,16 +2,12 @@ import torch
 from torch import nn
 from transformers import AutoModelForAudioClassification, Wav2Vec2FeatureExtractor
 
-
-MODEL_NAME = "facebook/wav2vec2-base"
-feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(MODEL_NAME)
-
 class HFAutoClassifier(torch.nn.Module):
     def __init__(self, num_classes, **kwargs):
         super().__init__()
         self.num_classes = num_classes
         self.model_name = kwargs.get('model_name')
-        self.model = AutoModelForAudioClassification.from_pretrained(MODEL_NAME)
+        self.model = AutoModelForAudioClassification.from_pretrained(self.model_name)
         self.model.classifier = nn.Identity()
         self.classifier = nn.Linear(256, self.num_classes)
 
