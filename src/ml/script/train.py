163161ff571485f1996ab3f9d12d61ecd4f96992
onetwothr1 <kimjh1101@snu.ac.kr>
2023-11-26 10:56:58 +0000

    Add precision configuration

--- src/ml/script/train.py
+++ src/ml/script/train.py
@@ -3,7 +3,7 @@ import wandb
 import lightning as L
 from lightning.pytorch import seed_everything
 from lightning.pytorch.loggers import WandbLogger
-from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
+from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor, DeviceStatsMonitor
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
 sys.path.append('/home/elicer/project/src/ml')
@@ -16,18 +16,27 @@ from util.constants import *
 
 # -------- parsing argument --------
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--name', dest='name', default=None, required=True)
-
-# to resume training from a checkpoint
-parser.add_argument('-c', '--ckptpath', dest='ckpt_path', default=None) 
-parser.add_argument('--run-id', dest='wandb_run_id', default=None)
+parser.add_argument('-n', '--name', dest='wandb_run_name', default='') # wandb run name. If None, do not use wandb logger
+parser.add_argument('-c', '--ckptpath', dest='ckpt_path', default=None) # model checkpoint to resume training
+parser.add_argument('--run-id', dest='wandb_run_id', default=None) # wandb run-id to resume training
 
 args = parser.parse_args()
 
 
-# -------------- main --------------
+# ----------- preparation ----------
 seed_everything(CFG['seed_everything'], workers=True)
 
+if args.wandb_run_name:
+    wandb.login(key = WANDB_API_KEY)
+    wandb_logger = WandbLogger(
+                    project = CFG['project'],
+                    name = args.wandb_run_name,
+                    id = args.wandb_run_id,
+                    config = CFG,
+                    )
+
+
+# -------------- main --------------
 net_name = CFG['model']['class_path']
 net = globals()[net_name](**CFG['model'][net_name]['init_args'])
 
@@ -52,26 +61,19 @@ datamodule = LitDataModule(
                 collate_fn = transform.collate_fn if hasattr(transform, 'collate_fn') else None
                 )
 
-wandb.login(key = WANDB_API_KEY)
-wandb_logger = WandbLogger(
-                project = CFG['project'],
-                name = args.name,
-                id = args.wandb_run_id,
-                config = CFG,
-                )
-
 trainer = L.Trainer(
-                logger = wandb_logger,
+                logger = wandb_logger if args.wandb_run_name else None,
                 max_epochs = CFG['trainer']['n_epoch'],
                 accelerator = CFG['trainer']['accelerator'],
                 check_val_every_n_epoch = CFG['trainer']['check_val_every_n_epoch'],
-                deterministic = True, # MUST BE FALSE WHEN ITS WAV2VEC2. FATAL ERROR RAISES
+                precision = CFG['trainer']['precision'],
+                deterministic = False, # MUST BE FALSE WHEN ITS WAV2VEC2. FATAL ERROR RAISES
                 callbacks = [
                     ModelCheckpoint(
                         save_top_k = 5,
                         monitor = 'val/loss',
                         mode = 'min',
-                        dirpath = EXPERIMENTS_DIR + os.sep + CFG['project'] + os.sep + args.name,
+                        dirpath = EXPERIMENTS_DIR + os.sep + CFG['project'] + os.sep + args.wandb_run_name,
                         filename = "{epoch:02d}-{val_loss:.4f}"
                     ),
                     EarlyStopping(
@@ -80,7 +82,8 @@ trainer = L.Trainer(
                         min_delta = 1e-4,
                         patience = 4,
                     ),
-                    LearningRateMonitor(logging_interval = 'epoch')
+                    LearningRateMonitor(logging_interval = 'epoch'),
+                    DeviceStatsMonitor()
                 ],
                 num_sanity_val_steps = 1
                 )
